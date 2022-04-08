2f60fc37d99c20d7986e1584c4e96d69fe6151e8
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-08 20:48:49 +0300

    Other changes

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Student, Unit, Lecturer, Attendance
 # Register your models here.
+
+
+admin.site.register(Student)
+admin.site.register(Unit)
+admin.site.register(Lecturer)
+admin.site.register(Attendance)
--- attendance/models.py
+++ attendance/models.py
@@ -22,7 +22,7 @@ class Lecturer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    def __str__(slef):
+    def __str__(self):
         return self.first_name + " " + self.last_name
 
 
