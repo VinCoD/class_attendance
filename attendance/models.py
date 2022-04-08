c3ea5cdfe07bb367ac65c3400590b508bca5c052
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-08 20:38:10 +0300

    Created Student Model

--- attendance/models.py
+++ attendance/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Student(models.Model)
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    registeration_number = models.CharField(max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.registeration_number
+
 class Unit(models.Model):
     unit_code = models.CharField(max_length=50, primary_key=True)
     unit_name = models.CharField(max_length=150)
@@ -10,12 +17,14 @@ class Unit(models.Model):
     def __str__(self):
         return self.unit_code + ": " + self.unit_name
 
+
 class Lecturer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
     def __str__(slef):
         return self.first_name + " " + self.last_name
+
 
 class Attendance(models.Model):
     venue = models.CharField(max_length=100)
