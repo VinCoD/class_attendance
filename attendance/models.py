59d83614804fdcbef7141a678b63d6b49373b741
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-08 20:21:27 +0300

    Created Lecturer model

--- attendance/models.py
+++ attendance/models.py
@@ -11,4 +11,9 @@ class Unit(models.Model):
         return self.unit_code + ": " + self.unit_name
 
 class Lecturer(models.Model):
-    
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(slef):
+        return self.first_name + " " + self.last_name
+
