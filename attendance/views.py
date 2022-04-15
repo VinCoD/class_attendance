24ccd375285c436d6a58f3a7b5a1164aec13f84b
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-15 20:09:46 +0300

    Created attendance view

--- attendance/views.py
+++ attendance/views.py
@@ -8,3 +8,11 @@ def index(request):
     attendances = Attendance.objects.all()
     context = {"attendances":attendances}
     return render(request, 'attendance/index.html', context)
+
+
+def attendance(request, id):
+    attendace = Attendance.objects.get(id=id)
+    context = {"attendace":attendace}
+
+    return(request, 'attendance/attendance.html', context)
+    
