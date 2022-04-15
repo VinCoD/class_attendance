f5d62f2328f3c7af63b81e899c0b930a5f9e1fb4
VinCoD <5895.2020@students.ku.ac.ke>
2022-04-15 19:06:08 +0300

    Added a root templates folder

--- class_attendance/settings.py
+++ class_attendance/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'class_attendance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
